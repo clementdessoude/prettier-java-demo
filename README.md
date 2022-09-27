923de05a9bb1938e07f36043d8449f79ebbbc78f
Clement Dessoude <clement.dessoude@gmail.com>
2022-09-27 11:05:19 +0200

    chore: update prettier versions and snippet

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 When running:
+
 ```bash
 npm install
 
 node index.js
 ```
-
-I got this error:
-
-<img width="715" alt="Screen Shot 2021-10-02 at 10 35 09" src="https://user-images.githubusercontent.com/25530077/135709471-6ca7b06c-d8af-45ef-8e85-44e008ea955a.png">
--- index.js
+++ index.js
@@ -2,24 +2,61 @@ const prettier = require("prettier");
 const path = require("path");
 
 const input = `
-package com.my.package;
-    import com.my.package.TestThing
-    import lombok.Getter;
-    public class TestClass {
-    @Getter private final TestThing testThing;
-    public MsgNoNamespace(testThing TestThing) {
-    this.testThing = testThing;
-    }
-    }
+class T {
+
+  @DELETE
+  @Path("/signapprovers/{projectKey}/{repoSlug}")
+  @Consumes({ MediaType.APPLICATION_JSON })
+  @PermissionRequired({ Permission.REPO_ADMIN })
+  public Response deleteSignapprovers(
+    @PathParam("projectKey") final String projectKey,
+    @PathParam("repoSlug") final String repoSlug,
+    RestBranchSignapprovers signapprovers
+  ) {
+    Repository repository = repositoryService.getBySlug(projectKey, repoSlug);
+    RestCommand c = new RestCommand(
+      authenticationContext,
+      permissionService,
+      repository.getProject().getId(),
+      repository.getId()
+    ) {
+      @Override
+      Response doExecute() {
+        dao
+          .getSignapprovers(projectKey, Optional.of(repoSlug))
+          .ifPresent(l -> { // <<<<<<<<<< OK
+            l.remove(signapprovers);
+            dao.setSignapprovers(projectKey, Optional.of(repoSlug), l);
+            try {
+              auditEventPublisher.publishRepositoryAuditEvent(
+                projectKey,
+                repoSlug,
+                signApproverJsonUtil.serializeNonNull(signapprovers),
+                WorkzoneConfigChangedEvents.WorkzoneSignApproversConfigDeletedEvent.name()
+              );
+            } catch (IOException e) {
+              log.warn(e.getMessage(), e);
+            }
+          });
+        return Response.ok().build();
+      }
+    };
+    return c.execute();
+  }
+}
 `;
 
 try {
-  prettier.format(input, {
+  const output = prettier.format(input, {
     parser: "java",
     plugins: [path.resolve(__dirname)],
     tabWidth: 2,
-    endOfLine: "lf",
+    semi: false,
+    trailingComma: "es5",
+    singleQuote: true,
+    printWidth: 100,
   });
+  console.log(output);
 } catch (err) {
   console.log(err);
 }
